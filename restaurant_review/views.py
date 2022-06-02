9fe5fbeb2467a19d9e4c53df7eef997cb9899fc4
UncleAyo <ayosamobitade@gmail.com>
2022-06-02 15:15:42 +0100

    some updates

--- restaurant_review/views.py
+++ restaurant_review/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .review_module import review_model
 
 # Create your views here.
 
 def ReviewPageView(request):
-    pred = 0
+    pred = ''
+    if request.method == 'POST':
+        if request.POST.get('pred_button'):
+            text = request.POST['review_text']
+            model = review_model('model', 'vectorizer')
+            model.clean_data(str(text))
+            pred = model.predict()
+        else:
+            redirect('reviewpage')
+    else:
+        print('Error Occured')
+
     return render(request, 'review/review.html',
     {'pred':pred
     
